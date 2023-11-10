dc2a0c3a39a408ac01f62baa8adbe843d70dd5cd
Marc Mendoza <777marc@gmail.com>
2023-11-10 13:51:38 -0500

    dev setup

--- src/index.js
+++ src/index.js
@@ -13,5 +13,5 @@ app.get("/", (req, res) => {
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}.`);
 });
