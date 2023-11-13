d89723e6fbdd94e61afd644819b153bcb1064dfd
Marc Mendoza <777marc@gmail.com>
2023-11-13 15:20:49 -0500

    clean up

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,10 +4,6 @@ socket.on("newConnections", (message) => {
   console.log(`the server says:${message}`);
 });
 
-// document.querySelector("#increment").addEventListener("click", () => {
-//   console.log("clicked");
-//   socket.emit("increment");
-// });
 document.getElementById("myForm").addEventListener("submit", function (event) {
   // Prevent the default form submission
   event.preventDefault();
@@ -16,10 +12,7 @@ document.getElementById("myForm").addEventListener("submit", function (event) {
   var inputValue = document.getElementById("inputField").value;
 
   // Perform any action with the input value (for example, log it to the console)
-  console.log("Submitted value:", inputValue);
   socket.emit("newMessage", inputValue);
-
-  // You can add more code here to handle the form submission as needed
 });
 
 socket.on("serverResponse", (res) => {
--- src/index.js
+++ src/index.js
@@ -12,18 +12,10 @@ const publicDirectoryPath = path.join(__dirname, "../public");
 
 app.use(express.static(publicDirectoryPath));
 
-let count = 0;
-
 io.on("connection", (socket) => {
   console.log("new connection");
 
   socket.emit("newConnections", "Welcome");
-
-  // socket.on("increment", () => {
-  //   count++;
-  //   // gobal emit to all connected clients
-  //   io.emit("countUpdated", count);
-  // });
 
   socket.on("newMessage", (message) => {
     io.emit("serverResponse", `the server recieved: ${message}.`);
