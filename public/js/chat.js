1c74e4be958d055f345873baa5c1a696e7e91d15
Marc Mendoza <777marc@gmail.com>
2023-11-29 10:07:57 -0500

    clean up

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,34 +1,37 @@
 const socket = io();
 
+// elements
+const $messageForm = document.getElementById("message-form");
+const $messageFormInput = document.getElementById("input");
+const $messageFormButton = document.getElementById("submit");
+const $sendLocationButton = document.getElementById("send-location");
+
 socket.on("newConnections", (message) => {
   console.log(`the server says:${message}`);
 });
 
-document
-  .getElementById("message-form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    document.getElementById("submit").disabled = true;
+$messageForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  $messageFormButton.disabled = true;
 
-    var inputValue = document.getElementById("inputField").value;
+  var message = $messageFormInput.value;
 
-    socket.emit("newMessage", inputValue, () => {
-      console.log("server acknowledged");
-      document.getElementById("inputField").value = "";
-      document.getElementById("inputField").focus();
-      document.getElementById("submit").disabled = false;
-      addItem(inputValue, "sentMessages");
-    });
+  socket.emit("newMessage", message, () => {
+    console.log("server acknowledged");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+    $messageFormButton.disabled = false;
   });
+});
 
 socket.on("serverResponse", (res) => {
   addItem(res, "recievedMessages");
 });
 
-document.querySelector("#sendLocation").addEventListener("click", () => {
-  document.getElementById("sendLocation").disabled = true;
+$sendLocationButton.addEventListener("click", () => {
+  $sendLocationButton.disabled = true;
   if (!navigator.geolocation) {
-    document.getElementById("sendLocation").disabled = false;
+    $sendLocationButton.disabled = false;
     return alert("geolocation not supported by your browser");
   }
 
@@ -37,22 +40,7 @@ document.querySelector("#sendLocation").addEventListener("click", () => {
     const { latitude, longitude } = position.coords;
     socket.emit("sendLocation", { latitude, longitude }, () => {
       console.log("server ack receipt");
-      document.getElementById("sendLocation").disabled = false;
+      $sendLocationButton.disabled = false;
     });
   });
 });
-
-function addItem(message, list) {
-  // Get the UL element
-  var ulElement = document.getElementById(list);
-
-  // Create a new list item
-  var liElement = document.createElement("li");
-
-  // Set the text content of the list item (you can modify this as needed)
-  var itemText = document.createTextNode(message);
-  liElement.appendChild(itemText);
-
-  // Append the new list item to the UL
-  ulElement.appendChild(liElement);
-}
